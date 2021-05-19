b45c975718cf8a755714c2acd97a56f19e3035ed
PerizatKurmanbaeva <perizat111203@mail.ru>
2021-05-19 22:17:25 +0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Secret Agent</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    body {
+      background-color: #ffd11a;
+      padding: 25px;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #ffe680;
+      padding: 25px;
+    }
+  </style>
+</head>
+<body>
+
+
+
+<div class="container text-center" >
+	<h2>My Portfolio</h2>      
+	<p>You don't have to know my name</p>
+	<img src="D:\college\comnet\site\photo\CharlizeTheron_gun.gif" style="width:15%" alt="Image">
+	<p>But ...</p>
+	  
+</div>
+  
+<div class="container-fluid bg-3 text-center">    
+  <h3>You have to watch this video</h3><br>
+  <iframe width="450" height="255" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div><br>
+
+
+</body>
+</html>
